85d86eb3138f0d6faa562c71765d2a2cc36b5704
ecovaci <ecovaci@griddynamics.com>
2023-11-02 17:40:20 +0200

    additional eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,44 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+  ignorePatterns: ["dist", "node_modules", ".eslintrc.cjs", "vite-env.d.ts"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "import"],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "import/order": [
+          "error",
+          {
+            groups: [
+              ["builtin", "external"],
+              "internal",
+              ["parent", "sibling", "index"],
+            ],
+            "newlines-between": "always",
+            alphabetize: {
+              order: "asc",
+            },
+          },
+        ],
+        "react-refresh/only-export-components": [
+          "warn",
+          { allowConstantExport: true },
+        ],
+        "no-shadow": "off",
+        "@typescript-eslint/no-shadow": ["error"],
+        "@typescript-eslint/no-unused-vars": [
+          "error",
+          { varsIgnorePattern: "^_" },
+        ],
+        "@typescript-eslint/default-param-last": "error",
+        "@typescript-eslint/no-floating-promises": "off",
+      },
+    },
+  ],
+};
