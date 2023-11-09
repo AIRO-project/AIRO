04df25b5013f0440c8a5b06e219c85b8ce634f8d
voleaninM <voleaninmax@gmail.com>
2023-11-09 13:03:05 +0200

    google sign in and sign out

---
+++ src/auth/handleSignIn.ts
@@ -0,0 +1,15 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+import { auth } from "../config/firebase";
+
+const googleSignIn = () => {
+  const provider = new GoogleAuthProvider();
+  signInWithPopup(auth, provider);
+};
+export const handleSignIn = async () => {
+  try {
+    await googleSignIn();
+  } catch (error) {
+    console.log("SignIn error", error);
+  }
+};
