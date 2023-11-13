e5009494afb560a649933f03e1888bd0802f5291
ecovaci <ecovaci@griddynamics.com>
2023-11-13 13:16:14 +0200

    redux toolkit setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
 import App from "./App";
+import { store } from "./state/store";
 import GlobalStyles from "./styles/global";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <GlobalStyles />
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
