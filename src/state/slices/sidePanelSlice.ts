7808ef7cf5eace52a92b530e625d10e1f03edaa1
dunitrashuk <mamaligadumitras@gmail.com>
2023-11-13 17:52:20 +0200

    side-panel redux state

---
+++ src/state/slices/sidePanelSlice.ts
@@ -0,0 +1,24 @@
+// src/features/sidePanelSlice.ts
+import { createSlice } from "@reduxjs/toolkit";
+
+interface SidePanelState {
+  isOpen: boolean;
+}
+
+const initialState: SidePanelState = {
+  isOpen: true,
+};
+
+const sidePanelSlice = createSlice({
+  name: "sidePanel",
+  initialState,
+
+  reducers: {
+    toggleSidePanel: (state) => {
+      state.isOpen = !state.isOpen;
+    },
+  },
+});
+
+export const { toggleSidePanel } = sidePanelSlice.actions;
+export default sidePanelSlice.reducer;
--- src/state/store/index.ts
+++ src/state/store/index.ts
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import sidePanelReducer from "../slices/sidePanelSlice";
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    sidePanel: sidePanelReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
-
 export type AppDispatch = typeof store.dispatch;
