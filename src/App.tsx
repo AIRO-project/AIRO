e1dec08b22c50e772b2f451a7221a25764649d42
dunitrashuk <mamaligadumitras@gmail.com>
2023-11-13 17:53:25 +0200

    app setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import styled from "styled-components";
+
+import Navbar from "./components/ui/Navbar/Navbar";
+import SidePanel from "./components/ui/SidePanel/SidePanel";
+
+const StyledMap = styled.div`
+  position: relative;
+  height: calc(100vh - 6rem);
+  background-color: grey;
+`;
+
 function App() {
-  return <div>AIRO</div>;
+  return (
+    <>
+      <Navbar />
+
+      <StyledMap>
+        <SidePanel />
+      </StyledMap>
+    </>
+  );
 }
 
 export default App;
